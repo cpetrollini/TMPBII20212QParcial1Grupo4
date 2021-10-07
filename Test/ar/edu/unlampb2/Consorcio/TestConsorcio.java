c190a622017c604a7e5c0e8182d876644420d31c
sergioglant <andresglant@yahoo.com.ar>
2021-10-06 22:46:47 -0300

    Agregue la carpeta Test

---
+++ Test/ar/edu/unlampb2/Consorcio/TestConsorcio.java
@@ -0,0 +1,14 @@
+package ar.edu.unlampb2.Consorcio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestConsorcio {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
