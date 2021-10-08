90fa9d67f68e0926e1ebe0f0a075c6d3e192df48
Ain Ponce <ponce.ain@gmail.com>
2021-10-08 13:29:12 -0300

    Test PagarExpensas (Monoambiente)

--- Test/ar/edu/unlam/pb2/Consorcio/TestMonoambiente.java
+++ Test/ar/edu/unlam/pb2/Consorcio/TestMonoambiente.java
@@ -24,5 +24,45 @@ public class TestMonoambiente {
 		assertEquals(monto, dto.getPagoExtraordinario());
 
 	}
+	
+	@Test
+	public void QuePagueLaExpensaTotal() {
+		Integer piso = 1;
+		Integer numero = 4;
+		Boolean cochera = false;
+		Integer montoExtraordinario = 1000;
+		Integer pagoExpensas = 3000;
+		Integer montoTotalAPagarServicios = 500;
+		Integer valorBasicoDeExpensas = 1000;
+		Integer extraPorMonoambiente = 300;
+		Integer montoServicios = 700;
+		
+		Integer totalAPagar = montoTotalAPagarServicios + valorBasicoDeExpensas + extraPorMonoambiente + montoServicios;
+		
+		Integer valorEsperadoDeLaExpensa = 0;
+		
+		Monoambiente DepartamentoMonoambiente = new Monoambiente(piso, numero, cochera, montoExtraordinario);
+		DepartamentoMonoambiente.serviciosComunesAPagar(montoServicios);
+		DepartamentoMonoambiente.pagoDeExpensas(pagoExpensas);
+		
+		Integer valorObtenido = 0;
+		
+		assertEquals(valorEsperadoDeLaExpensa, valorObtenido);
+	}
+	
+	@Test
+	public void QuePagueLaExpensaDeFormaParcial() {
+		Integer piso = 1;
+		Integer numero = 4;
+		Boolean cochera = false;
+		Integer monto = 1000;
+		Integer valorEsperadoDeLaExpensa = 1000;
+		Integer pagoExpensas = 3000;
+		
+		Monoambiente DepartamentoMonoambiente = new Monoambiente(piso, numero, cochera, monto);
+		
+		
+		
+	}
 
 }
